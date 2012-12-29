12b3ff9a6309b6ce1253d3548c4b32610246ec78
betterphp <jacek@kuzemczak.co.uk>
2012-12-29 22:44:42 +0000

    Fixed whitespace

--- src/main/java/uk/co/jacekk/bukkit/worldrules/Permission.java
+++ src/main/java/uk/co/jacekk/bukkit/worldrules/Permission.java
@@ -7,6 +7,6 @@ import uk.co.jacekk.bukkit.baseplugin.v6.permissions.PluginPermission;
 public class Permission {
 	
 	public static final PluginPermission SHOW_RULES		= new PluginPermission("worldrules.rules.show",		PermissionDefault.TRUE,		"Allows the player to see the rules for the world thy are in.");
-	public static final PluginPermission RELOAD_RULES		= new PluginPermission("worldrules.rules.reload",	PermissionDefault.OP,		"Allows the player to reload the config file.");
+	public static final PluginPermission RELOAD_RULES	= new PluginPermission("worldrules.rules.reload",	PermissionDefault.OP,		"Allows the player to reload the config file.");
 	
 }
