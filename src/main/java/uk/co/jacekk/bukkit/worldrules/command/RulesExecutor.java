a20808b3ae34f5efa3c5e605a568bf478afe0ff2
Jacek <jacek@kuzemczak.co.uk>
2013-06-22 03:09:09 +0100

    Use sub-command system.

--- src/main/java/uk/co/jacekk/bukkit/worldrules/command/RulesExecutor.java
+++ src/main/java/uk/co/jacekk/bukkit/worldrules/command/RulesExecutor.java
@@ -10,6 +10,7 @@ import org.bukkit.entity.Player;
 import uk.co.jacekk.bukkit.baseplugin.command.BaseCommandExecutor;
 import uk.co.jacekk.bukkit.baseplugin.command.CommandHandler;
 import uk.co.jacekk.bukkit.baseplugin.command.CommandTabCompletion;
+import uk.co.jacekk.bukkit.baseplugin.command.SubCommandHandler;
 import uk.co.jacekk.bukkit.worldrules.Permission;
 import uk.co.jacekk.bukkit.worldrules.WorldRules;
 
@@ -22,25 +23,24 @@ public class RulesExecutor extends BaseCommandExecutor<WorldRules> {
 	@CommandHandler(names = {"worldrules", "wr"}, description = "Provides various commands for managing the rules", usage = "[option]")
 	@CommandTabCompletion({"reload"})
 	public void worldrules(CommandSender sender, String label, String[] args){
-		if (args.length == 0){
-			sender.sendMessage(ChatColor.RED + "Usage: /worldrules <option> [args]");
-			sender.sendMessage(ChatColor.RED + "Available Options:");
+		sender.sendMessage(ChatColor.RED + "Usage: /worldrules <option> [args]");
+		sender.sendMessage(ChatColor.RED + "Available Options:");
+		
+		if (Permission.RELOAD_RULES.has(sender)){
 			sender.sendMessage(ChatColor.RED + "  reload - Reloads the rules from the config file.");
+		}
+	}
+	
+	@SubCommandHandler(name = "reload", parent = "worldrules")
+	public void worldrulesReload(CommandSender sender, String label, String[] args){
+		if (!Permission.RELOAD_RULES.has(sender)){
+			sender.sendMessage(ChatColor.RED + "You do not have permission to use this command !");
 			return;
 		}
 		
-		String option = args[0];
+		plugin.config.reload();
 		
-		if (option.equalsIgnoreCase("reload") || option.equalsIgnoreCase("r")){
-			if (!Permission.RELOAD_RULES.has(sender)){
-				sender.sendMessage(ChatColor.RED + "You do not have permission to use this command !");
-				return;
-			}
-			
-			plugin.config.reload();
-			
-			sender.sendMessage(ChatColor.GREEN + "WorldRules configuration reloaded.");
-		}
+		sender.sendMessage(ChatColor.GREEN + "WorldRules configuration reloaded.");
 	}
 	
 	@CommandHandler(names = {"rules"}, description = "Shows the rules for the current world")
